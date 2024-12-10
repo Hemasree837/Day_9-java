3a13f09fd6973a0a13768fa67e5bf91a694612c6
Hemasree837 <hemasree837@gmail.com>
2024-12-10 12:22:28 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ForEach {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Orange","PineApple"};
+        for (String b:a)
+        System.out.print(b+", ");
+    }
+}
