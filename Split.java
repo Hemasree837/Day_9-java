6714cb53dd2e85ef8e5b2713102bf161a2b30852
Hemasree837 <hemasree837@gmail.com>
2024-12-10 12:22:46 +0530

    Add files via upload

---
+++ Split.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Split {
+     public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String sp[]=a.split(" ");
+        System.out.println(Arrays.toString(sp));
+}
+}
+/*
+ *hema sree
+[hema, sree]
+ */
